aefb51d5113e75b069da97bb39ac8812b5aa18fe
prasetyobella17 <prasetyo.bella@ancol.com>
2023-03-23 08:21:51 +0700

    first commit

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test Command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // return 0;
+        $data = [
+            'name' => 'John 10',
+            'email' => 'johndoe10@example.com',
+            'password' => bcrypt('secret'),
+        ];
+
+        DB::table('users')->insert($data);
+
+        $this->info('Data inserted successfully.');
+    }
+}
---
+++ app/Console/Kernel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console;
+
+use App\User;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $data = array(
+                'name' => rand(0, 10000),
+                'email' => rand(0, 10000),
+                'password' => bcrypt(rand(0, 10000)),
+            );
+            User::create($data);
+        })->everyMinute()->appendOutputTo('scheduler.log');
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
